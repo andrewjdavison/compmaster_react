8b0ae3d3f63217c02539cff4cda2ab7a79300df2
Andrew Davison <software@davison-family.com>
2018-04-09 19:54:13 +1000

    Added a new util put method

--- client/src/coreutil.js
+++ client/src/coreutil.js
@@ -45,3 +45,49 @@ export function post(url, data){
 };
 
 
+export function get(url){
+  let postResponse={
+    body: undefined,
+    error:undefined,
+  };
+
+  let headers = new Headers();
+
+  headers.append('Accept', 'application/json');
+  headers.append('Content-Type','application/json');
+
+  let config={
+    method: 'GET',
+    headers: headers,
+  };
+
+  return fetch(url, config)
+    .then((response) => {
+      if(response.status !== 200){
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+  .then((responseJSON)=>{
+      postResponse.body = responseJSON;
+      return postResponse;
+    })
+  .catch((error)=>{
+    console.log('error');
+    if(error.message===401){
+      postResponse.error={
+        code:401,
+        summary:'Unauthorised'
+      };
+    }
+    else {
+      postResponse.error={
+        code: error.message
+      };
+    }
+
+    return postResponse;
+  });
+};
+
+
